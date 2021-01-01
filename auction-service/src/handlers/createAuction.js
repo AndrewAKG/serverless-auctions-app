a9db45b63b2092578b5dcc3cb76ebc531c61b008
AndrewAKG <andrewakram1997@gmail.com>
2021-01-01 22:08:16 +0200

    feat: added auctions table, added sub folders for iam, resources, createAuction completed

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,8 +1,28 @@
-async function createAuction(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const createAuction = async (event, context) => {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
   };
-}
+
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+};
 
 export const handler = createAuction;
