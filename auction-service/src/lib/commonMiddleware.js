b6ee3d9331d5ae557d620ff9a5ccb61065ade0ee
AndrewAKG <andrewakram1997@gmail.com>
2021-01-04 22:53:45 +0200

    feat: adding @middy/http-cors and adding allow origin to *

--- auction-service/src/lib/commonMiddleware.js
+++ auction-service/src/lib/commonMiddleware.js
@@ -2,10 +2,12 @@ import middy from '@middy/core';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 import httpErrorHandler from 'middy-middleware-json-error-handler';
+import httpCors from '@middy/http-cors';
 
 export default (handler) =>
   middy(handler).use([
     httpJsonBodyParser(),
     httpEventNormalizer(),
-    httpErrorHandler()
+    httpErrorHandler(),
+    httpCors()
   ]);
